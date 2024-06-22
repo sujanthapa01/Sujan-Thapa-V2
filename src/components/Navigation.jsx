0e0e0803a301c7f951c33fccfd735329f27a9e66
sujanthapa01 <sujanthapa4001@gmail.com>
2024-06-22 16:19:39 +0530

    ui improvements

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import activeHome from "../assets/active-home.png"; // Importing instagram image
 import home from "../assets/Home-icon.png";
 import projects from "../assets/Projects-icon.png";
@@ -14,70 +15,74 @@ function Navigation() {
   return (
     <div className="fixed w-full md:sticky bottom-0 md:top-0 h-16 md:w-24 shrink-0 md:h-screen overflow-y-auto no-scrollbar border-r dark:border-slate-800 z-50 backdrop-filter backdrop-blur-lg">
       <div className="h-full w-full flex flex-row md:flex-col justify-ce items-center after:flex-1 after:mt-auto">
-    <div className="hidden md:block md:flex-1">
-      <div class="flex justify-center my-4"> <img src={myimg} height='32' width='32' className="rounded-full" alt="" srcset="" /></div>
-    </div>
-       
+        <div className="hidden md:block md:flex-1 ">
+          <div class="flex justify-center my-4 tooltip-container">
+          <span class="tooltip rounded-md">@SujanThapa</span>
+              <img src={myimg} height='32' width='32' className="rounded-full" data-tip="sujanthapa" alt="" srcset="" />
+            </div>
 
-<div className="flex-1 grow flex items-center w-full">
-  <nav className="w-full">
-    <ul className="md:space-y-4 flex flex-row items-center w-screen justify-evenly md:flex-col md:justify-start md:items-center md:w-24">
-      <li className="md:py-2"> <NavLink
-            to="/"
+        </div>
 
-            onClick={() => setActive("Home")}
-          >
-            {active === "Home" ? (
-              <img
-                className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
-                src={activeHome}
-                alt="Home"
-              />
-            ) : (
-              <img
-                className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
-                src={home}
-                alt="Home"
-              />
-            )}
-          </NavLink></li>
-      <li className="md:py-2"><NavLink to="/Projects" onClick={() => setActive("Projects")}>
-            {active === "Projects" ? (
-              <img
-                className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
-                src={Porjectactive}
-                alt="Projects"
-              />
-            ) : (
-              <img
-                className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
-                src={projects}
-                alt="About"
-              />
-            )}
-          </NavLink></li>
-      <li className="md:py-2"> <NavLink
-            to="/Aboutme"
 
-            onClick={() => setActive("Aboutme")}
-          >
-            {active === "Aboutme" ? (
-              <img
-                className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
-                src={Abotmeactive}
-                alt=""
-              />
-            ) : (
-              <img
-                className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
-                src={Aboutme}
-                alt="Aboutme"
-              />
-            )}
-          </NavLink></li>
-    </ul>
-  </nav>
-   </div>
+        <div className="flex-1 grow flex items-center w-full">
+          <nav className="w-full">
+            <ul className="md:space-y-4 flex flex-row items-center w-screen justify-evenly md:flex-col md:justify-start md:items-center md:w-24">
+              <li className="md:py-2"> <NavLink
+                to="/"
+
+                onClick={() => setActive("Home")}
+              >
+                {active === "Home" ? (
+                  <img
+                    className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
+                    src={activeHome}
+                    alt="Home"
+                  />
+                ) : (
+                  <img
+                    className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
+                    src={home}
+                    alt="Home"
+                  />
+                )}
+              </NavLink></li>
+              <li className="md:py-2"><NavLink to="/Projects" onClick={() => setActive("Projects")}>
+                {active === "Projects" ? (
+                  <img
+                    className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
+                    src={Porjectactive}
+                    alt="Projects"
+                  />
+                ) : (
+                  <img
+                    className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
+                    src={projects}
+                    alt="About"
+                  />
+                )}
+              </NavLink></li>
+              <li className="md:py-2"> <NavLink
+                to="/Aboutme"
+
+                onClick={() => setActive("Aboutme")}
+              >
+                {active === "Aboutme" ? (
+                  <img
+                    className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
+                    src={Abotmeactive}
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    className="hover:scale-[1.1] duration-200 cursor-pointer h-[28px]"
+                    src={Aboutme}
+                    alt="Aboutme"
+                  />
+                )}
+              </NavLink></li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,30 +6,30 @@ import ProjectComp from "../components/projectComp";
 
 function Projects() {
   return (
-    <main className="grow md:flex space-y-8 md:space-y-0 md:space-x-8 pt-12 md:pt-16 pb-16 md:pb-20">
+    <>
       {/* <Header /> */}
       <div className="grow md:flex space-y-8 md:space-y-0 md:space-x-8 pt-12 md:pt-16 pb-16 md:pb-20">
         <div className="grow">
           <div className="max-w-[700px]">
 
             <section>
-              <h1 className="font-bold text-[2.5rem] mb-12">Some Of My Projects</h1>
 
+              <h1 className="font-bold text-[2.5rem] mb-12 ">Some Of My Projects</h1>
               <div>
                 <ProjectComp />
               </div>
 
             </section>
           </div>
-            <aside className="md:w-[240px] lg:w-[300px] shrink-0">
-                <div className="xl:fixed lg:fixed md:fixed static top-[8rem] space-y-6">
-                  <Cards />
-                </div>
-              </aside>
         </div>
+        <aside className="md:w-[240px] lg:w-[300px] shrink-0">
+          <div className="xl:fixed lg:fixed md:fixed static top-[8rem] space-y-6">
+            <Cards />
+          </div>
+        </aside>
       </div>
-    </main>
-  );
+    </>
+  )
 }
 
 export default Projects;
