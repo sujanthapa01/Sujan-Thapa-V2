a330ae38693793474f074e718c2be125ec903af9
sujanthapa01 <sujanthapa4001@gmail.com>
2024-07-12 23:53:13 +0530

    bug fix

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -11,7 +11,7 @@ function Aboutme() {
             <h1 className='text-[2rem] md:text-[3rem] lg:text-[3rem] xl:text-[3rem] leading-[50px] mt-2 mb-8'>
               <strong>Hii. I'm <span className='text-blue-500 inline-flex relative duration-200 cursor-pointer before:absolute before:inset-0 before:bg-blue-300 before:opacity-30 before:-z-10 hover:before:-rotate-0 before:-rotate-3 before:translate-y-1/4'>Sujan Thapa </span> 🎸</strong>
             </h1>
-          
+
             <div className="relative w-full overflow-hidden rounded-md">
               <img
                 src={bioimg}
@@ -25,11 +25,19 @@ function Aboutme() {
             <div className='text-slate-500 dark:text-slate-400 space-y-8 mt-4'>
               <div className="space-y-4">
                 <h2 className="h2 font-aspekta text-slate-800 dark:text-slate-100">Short Bio</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam expedita dignissimos repellendus sunt ullam magni qui natus accusantium praesentium nam quidem sit molestias inventore, eos a quas odio libero eveniet ipsum assumenda laborum sequi odit. Accusamus, obcaecati unde! Consequuntur omnis velit, reprehenderit earum neque voluptatem vitae minus? Quam amet explicabo officiis dignissimos sequi delectus odio optio repudiandae, veniam reprehenderit nihil nisi enim itaque nobis!
+                <p>Hi, I’m Sujan Thapa! I’m currently studying for my <a href="https://www.google.com/search?q=bca+explanation&sca_esv=ffcfe78bb05bd262&sxsrf=ADLYWIL0J8pBlhvOWA4kd0G_sfzzQUKCsw%3A1720807650745&ei=4nCRZsCcLZnu1e8P_s_5mAE&oq=bca+expl&gs_lp=Egxnd3Mtd2l6LXNlcnAiCGJjYSBleHBsKgIIADIFEAAYgAQyBRAAGIAEMgUQABiABDIIEAAYFhgKGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB4yBhAAGBYYHjIGEAAYFhgeMgYQABgWGB5IwS9QAFixJnAFeACQAQCYAeECoAG_CaoBBzAuMi4yLjG4AQHIAQD4AQGYAgqgAvkJwgIEECMYJ8ICChAjGIAEGCcYigXCAg0QABiABBixAxhDGIoFwgIKEAAYgAQYQxiKBcICDRAuGIAEGLEDGEMYigXCAgUQIRigAcICBRAhGJ8FwgIKEAAYgAQYFBiHAsICDBAAGIAEGAIYChjLAcICCxAAGIAEGJECGIoFwgINEAAYgAQYsQMYFBiHApgDAJIHBzUuMi4xLjKgB9sX&sclient=gws-wiz-serp" className='text-blue-500'
+                >BCA</a> at <a href="https://dcedu.in/" className='text-blue-500'>  Dronacharya</a> College and deeply passionate about <a href="https://www.google.com/search?q=software+development&oq=software+development&gs_lcrp=EgZjaHJvbWUyDwgAEEUYORiDARixAxiABDIHCAEQABiABDIHCAIQABiABDIKCAMQABixAxiABDIHCAQQABiABDIKCAUQABixAxiABDIHCAYQABiABDIHCAcQABiABDIHCAgQABiABDIHCAkQABiABNIBCTExNTQwajBqNKgCALACAQ&sourceid=chrome&ie=UTF-8" className='text-blue-500'>software development </a>and <a href="https://www.google.com/search?q=software+engineering&sca_esv=ffcfe78bb05bd262&sxsrf=ADLYWILQVZMwrDYr94D8ktiW72REGDa4qQ%3A1720808255518&ei=P3ORZt6mH7bi2roPwMW-wAg&ved=0ahUKEwiewq-mjqKHAxU2sVYBHcCiD4gQ4dUDCA8&uact=5&oq=software+engineering&gs_lp=Egxnd3Mtd2l6LXNlcnAiFHNvZnR3YXJlIGVuZ2luZWVyaW5nMgoQIxiABBgnGIoFMgoQABiABBhDGIoFMggQABiABBixAzIKEAAYgAQYAhjLATIIEAAYgAQYsQMyChAAGIAEGEMYigUyDhAAGIAEGLEDGIMBGIoFMgoQABiABBgCGMsBMgUQABiABDIKEAAYgAQYAhjLAUiUBVCbA1ibA3ABeAGQAQCYAdMBoAHTAaoBAzItMbgBA8gBAPgBAZgCAqAC5wHCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBZgDAIgGAZAGCZIHBTEuMC4xoAe9CA&sclient=gws-wiz-serp" className='text-blue-500'>engineering</a>. As a <a href="https://www.google.com/search?q=full+stack+developer&oq=full+st&gs_lcrp=EgZjaHJvbWUqCggBEAAYsQMYgAQyDQgAEAAYgwEYsQMYgAQyCggBEAAYsQMYgAQyBwgCEAAYgAQyBwgDEAAYgAQyBggEEEUYOTIGCAUQRRg8MgYIBhBFGDwyBggHEEUYPdIBCDMyOTNqMGo5qAIAsAIA&sourceid=chrome&ie=UTF-8" className='text-blue-500'>full stack developer</a>, I focus on creating applications that provide seamless user experiences across all aspects. You can check out my <a href="https://github.com/sujanthapa01" className='text-blue-500'>GitHub</a> to see examples of the full stack applications I’ve developed. Outside of coding, I enjoy immersing myself in <a href="https://open.spotify.com/user/rgwfr9glols5ew1tdqkngs12x?si=f1d46095b68f4661" className='text-blue-500'>music</a>, which is my favorite way to unwind and recharge.
+
+
+
+
+
+
+
                 </p>
               </div>
             </div>
-  
+
           </section>
         </div>
       </div>
