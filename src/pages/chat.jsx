dbda39146037f2d7089ed52074b4b113ac80abe7
sujanthapa01 <sujanthapa4001@gmail.com>
2024-07-23 18:57:29 +0530

    commit

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -1,12 +1,21 @@
 import Dashboard from "./Dashboard";
-
+import Cards from "../components/cards";
 function chat() {
   return (
-    <div className='flex h-full justify-center items-center flex-col'>chat
-<br />
-<Dashboard/>
-
+    <div className='grow md:flex space-y-8 md:space-y-0 md:space-x-8 pt-12 md:pt-16 pb-16 md:pb-20'>
+    <div className='grow'>
+      <div className='min-w-[200px] md:min-w-0'>
+        <section>
+         <div className="flex justify-center items-center h-full"> chat</div>
+        </section>
+      </div>
     </div>
+    <aside className='md:w-[260px] lg:w-[300px] shrink-0'>
+      <div className="xl:fixed lg:fixed md:fixed static top-[8rem] md:w-[262px] lg:w-[300px] space-y-6">
+        <Cards />
+      </div>
+    </aside>
+  </div>
   )
 }
 
